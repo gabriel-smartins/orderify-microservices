a16e426fda5ec418ae63c2e8baf3c6751c04c3c7
Gabriel Martins <gab.smartinn@outlook.com>
2026-03-18 16:04:53 -0300

    feat(orderify-api): add Order entity with status mapping

---
+++ orderify-api/src/main/java/br/com/orderify/entity/Order.java
@@ -0,0 +1,27 @@
+package br.com.orderify.entity;
+
+import br.com.orderify.entity.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    private UUID id = UUID.randomUUID();
+    private String customer;
+    private List<OrderItem> orderItems = new ArrayList<>();
+    private Double totalValue;
+    private String notificationEmail;
+    private Status status;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dateHour;
+}
