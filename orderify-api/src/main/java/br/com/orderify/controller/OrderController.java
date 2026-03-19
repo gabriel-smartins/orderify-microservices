a27a1f2150f022bab4ef348a0d38bc22f661eef5
Gabriel Martins <gab.smartinn@outlook.com>
2026-03-19 00:55:11 -0300

    feat(orderify-api): add OrderService on OrderController

--- orderify-api/src/main/java/br/com/orderify/controller/OrderController.java
+++ orderify-api/src/main/java/br/com/orderify/controller/OrderController.java
@@ -1,11 +1,13 @@
 package br.com.orderify.controller;
 
 import br.com.orderify.entity.Order;
+import br.com.orderify.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -18,17 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Orders", description = "Resource to create a new order")
 @RestController
 @RequestMapping("/api/v1/orders")
+@RequiredArgsConstructor
 public class OrderController {
 
     private final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
+    private final OrderService orderService;
 
     @Operation(summary = "Create new order", description = "Contains operations to create a new order",
     responses = @ApiResponse(responseCode = "201", description = "Resource created successfully",
     content = @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class))))
     @PostMapping
     public ResponseEntity<Order> createOrder(@RequestBody Order order){
-
         logger.info("Order received: {}", order.toString());
+        order = orderService.queueOrder(order);
         return ResponseEntity.status(HttpStatus.CREATED).body(order);
     }
 }
