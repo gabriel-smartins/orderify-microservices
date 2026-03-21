1ebd93de8534e1f94bfe6307fff642e96425c0df
Gabriel Martins <gab.smartinn@outlook.com>
2026-03-21 02:05:15 -0300

    feat(processor): implement JPA repositories for order and product entities

---
+++ processor/src/main/java/br/com/orderify/processor/repository/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package br.com.orderify.processor.repository;
+
+import br.com.orderify.processor.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, UUID> {
+}
---
+++ processor/src/main/java/br/com/orderify/processor/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package br.com.orderify.processor.repository;
+
+import br.com.orderify.processor.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
