5527381b99c9b265ce90bb5c46b605a500992413
Gabriel Martins <gab.smartinn@outlook.com>
2026-03-23 14:57:27 -0300

    feat(processor): integrate OrderService into OrderListener to process messages

--- processor/src/main/java/br/com/orderify/processor/listener/OrderListener.java
+++ processor/src/main/java/br/com/orderify/processor/listener/OrderListener.java
@@ -2,17 +2,19 @@ package br.com.orderify.processor.listener;
 
 import br.com.orderify.processor.entity.Order;
 import br.com.orderify.processor.entity.enums.Status;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import br.com.orderify.processor.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class OrderListener {
-
-    private final Logger logger = LoggerFactory.getLogger(OrderListener.class);
+    private final OrderService orderService;
 
     @RabbitListener(queues = "orders.v1.order-created.generate-processor")
     public void saveOrder(Order order) {
         order.setStatus(Status.PROCESSED);
-        logger.info("Order received: {}", order);
+        orderService.save(order);
     }
 }
